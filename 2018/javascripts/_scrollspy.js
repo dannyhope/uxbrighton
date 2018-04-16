da0445cdd1a16dca11ccb42f25598b6a07f97148
Jay Caines-Gooby <jay@gooby.org>
2018-04-16 13:11:02 +0100

    Paul's updates

--- 2018/javascripts/_scrollspy.js
+++ 2018/javascripts/_scrollspy.js
@@ -100,6 +100,11 @@
         && (offsets[i + 1] === undefined || scrollTop < offsets[i + 1])
         && this.activate(targets[i])
     }
+    if ($(document).scrollTop() > 20) {
+      $('body').addClass('scroll-started');
+    } else {
+      $('body').removeClass('scroll-started');
+    }
   }
 
   ScrollSpy.prototype.activate = function (target) {
--- 2018/javascripts/scripts.js
+++ 2018/javascripts/scripts.js
@@ -10,32 +10,32 @@ $(function() {
   $(".container").fitVids();
 
   // setup scroll based on height of fixed nav
-  var navBorderTop      = 1;
-  var navHeight         = $('.masthead').height();
-  var navScrollOffset   = navHeight + navBorderTop;
+  // TODO after resize / mq
+  if ( $( window ).width() > 699) {
+    var navOffset       = $('.masthead').height() - 1; 
+  } else {
+    var navOffset       = 0; 
+  }
+  var navScrollOffset   = navOffset + 1;
   var navScrollSpeed    = 900;
 
   // bootstrap scrollspy, automatically highlights the menu
-  $('body').scrollspy({ target: '.masthead__links',offset: navHeight });
+  $('body').scrollspy({ target: '.masthead__links',offset: navScrollOffset });
 
   // smoothly scroll to in-page links, uses request animation frame for smoothness
   $(document).on('click touchstart', 'a[href^="#"]:not([href="#"])', function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
     var target = $(this.hash);
+    console.log('target',target);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    var _goto = target.offset().top - navBorderTop;
+    var _goto = target.offset().top - navOffset;
+    console.log('_goto',_goto);
       if (target.length) {
         scrollTo(_goto, null, navScrollSpeed);
       }
     }
   });
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 30) {
-      $('.masthead').addClass('logo-shrink');
-    } else {
-      $('.masthead').removeClass('logo-shrink');
-    }
-  });
+
 
   // if(document.location.search.length) { $("#styleguide").load("style-guide.html"); };
 
