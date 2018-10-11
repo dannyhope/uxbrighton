ab79454f86ef7c84669b25088593367bb7b7d112
Jay Caines-Gooby <jay@gooby.org>
2018-10-11 21:45:32 +0100

    re-concat the min.js, strike out one of the 4 star sponsors

--- 2018/javascripts/scripts.js
+++ 2018/javascripts/scripts.js
@@ -12,9 +12,9 @@ $(function() {
   // setup scroll based on height of fixed nav
   // TODO after resize / mq
   if ( $( window ).width() > 699) {
-    var navOffset       = $('.masthead').height() - 1; 
+    var navOffset       = $('.masthead').height() - 1;
   } else {
-    var navOffset       = 0; 
+    var navOffset       = 0;
   }
   var navScrollOffset   = navOffset + 1;
   var navScrollSpeed    = 300;
@@ -66,3 +66,28 @@ d.withCredentials&&""!==k&&k!==m?XDomainRequest||void 0:XMLHttpRequest}return d}
 "true"),b.style.position="absolute",b.style.width=0,b.style.height=0,b.style.overflow="hidden",c.insertBefore(b,c.firstChild);d()}}function n(a){return function(){a.onerror=null;a.ontimeout=null;d()}}var a,b,q=0;m();var f=document.getElementsByTagName("use");for(b=0;b<f.length;b+=1){try{var h=f[b].getBoundingClientRect()}catch(w){h=!1}var g=(a=f[b].getAttribute("href")||f[b].getAttributeNS("http://www.w3.org/1999/xlink","href")||f[b].getAttribute("xlink:href"))&&a.split?a.split("#"):["",""];var c=
 g[0];g=g[1];var r=h&&0===h.left&&0===h.right&&0===h.top&&0===h.bottom;h&&0===h.width&&0===h.height&&!r?(c.length||!g||document.getElementById(g)||(c=""),f[b].hasAttribute("href")&&f[b].setAttributeNS("http://www.w3.org/1999/xlink","xlink:href",a),c.length&&(a=e[c],!0!==a&&setTimeout(l({useEl:f[b],base:c,hash:g}),0),void 0===a&&(g=u(c),void 0!==g&&(a=new g,e[c]=a,a.onload=p(a),a.onerror=n(a),a.ontimeout=n(a),a.open("GET",c),a.send(),q+=1)))):r?c.length&&e[c]&&setTimeout(l({useEl:f[b],base:c,
 hash:g}),0):void 0===e[c]?e[c]=!0:e[c].onload&&(e[c].abort(),delete e[c].onload,e[c]=!0)}f="";q+=1;d()};var p=function(){window.removeEventListener("load",p,!1);l=setTimeout(n,0)};"complete"!==document.readyState?window.addEventListener("load",p,!1):p()}})();
+
+/* Table of contents */
+$(document).ready(function(){
+  var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<strong>On this page</strong>" +
+    "<ul>";
+   var newLine, el, title, link;
+   $("#info h3[id]").each(function() {
+   el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+   newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+   ToC += newLine;
+   });
+   ToC +=
+   "</ul>" +
+  "</nav>";
+   $("[data-toc]").after(ToC);
+});
