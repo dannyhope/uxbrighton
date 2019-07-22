8a7f3aa784b743c7d177e1545135c28c26862bf2
eniallator <eniallatormc@gmail.com>
2019-07-22 13:53:56 +0100

    Added an accessibility scanner for checking the uxbrighton sitemap

---
+++ accessibility-scan/accessibility-scan.py
@@ -0,0 +1,64 @@
+import os
+import re
+import requests
+from xml.etree.ElementTree import fromstring
+from xmljson import badgerfish as bf
+
+# Specify your id from first registering on https://achecker.ca/checker/index.php and copy/pasting the ID
+# into a Python script file that has: ID = 'YOUR_ID_HERE'
+from settings import ID
+
+API_LINK = f'https://achecker.ca/checkacc.php?id={ID}'
+OUTPUT_DIR = os.path.join(os.getcwd(), 'output')
+NUM_URLS_TO_CHECK = None
+
+if os.path.isfile(OUTPUT_DIR):
+    raise Exception("Specified output directory is a file")
+elif not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+
+with open('./sitemap.xml', 'r') as sitemap:
+    contents = sitemap.read()
+
+data = bf.data(fromstring(contents))
+
+
+def find_urls(data):
+    if type(data) == list or not data:
+        return data
+    else:
+        for key in data:
+            output = find_urls(data[key])
+            if output:
+                return output
+
+
+url_data = find_urls(data)
+urls = []
+
+
+for url in url_data:
+    for key in url:
+        urls.append(url[key]['$'])
+
+
+for i in range(NUM_URLS_TO_CHECK if NUM_URLS_TO_CHECK else len(url_data)):
+    url = urls[i]
+    print('\nChecking ' + url)
+    response = requests.get(API_LINK + '&output=rest&uri=' + url)
+    cleaned_content = re.sub(rb'&[a-z]+;', lambda match: b'&amp;' + match.group(0)[1:], response.content)
+    xml_response = bf.data(fromstring(cleaned_content))
+    summary = xml_response['resultset']['summary']
+    if summary['status']['$'] == 'PASS':
+        print('Passed! moving on ...')
+        continue
+    print('Failed with ' + str(summary['NumOfErrors']['$']) + ' errors, ' + str(summary['NumOfLikelyProblems']['$'])
+          + ' likely problems and ' + str(summary['NumOfPotentialProblems']['$']) + ' potential problems')
+
+    response = requests.get(API_LINK + '&uri=' + url)
+    file_name = ''.join([char for char in url if char.isalnum()]) + '.html'
+    file_loc = os.path.join(OUTPUT_DIR, file_name)
+    print('Saving output to file under: ' + file_name)
+    with open(file_loc, 'wb') as f:
+        f.write(response.content)
