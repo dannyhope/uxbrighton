e75cc56df1e0043ca5a04e630e77ce39891e914f
Paul D <info@actino.co.uk>
2019-12-22 22:25:39 +0000

    fix to console errors and notifications

--- 2018/javascripts/scripts.js
+++ 2018/javascripts/scripts.js
@@ -4,14 +4,31 @@
 // @codekit-prepend "_stickybits.js";
 
 
+// COOKIES
+
+function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
+function setCookie(name, value, days) {
+    var d = new Date;
+    d.setTime(d.getTime() + 24*60*60*1000*days);
+    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+}
+function deleteCookie(name) { 
+  setCookie(name, '', -1); 
+}
+
+
+
+
+
+
 $(function() {
 
   // console.log('dom2');
 
-  function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-  }
+
 
   // stickybits('.sticky', {useStickyClasses: true});
 
