01ed04de6bdc0867d34adcd8b3d5a4c6718c2547
Bruce Taylor <brootaylor@gmail.com>
2023-04-17 17:45:29 +0100

    Latest email sub code and changes

--- scripts/components/emailSubscription.js
+++ scripts/components/emailSubscription.js
@@ -1,6 +1,16 @@
 (function() {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbyFLPMiKDWQbsrwnbdIAnxL2GzvAv0hlKJrXypsZbCQ_4BIGWzFPhDHGJJTOkNCznXn/exec'
   const form = document.forms['google-sheet']
+  const isJobsPage = document.querySelector('.email-form--jobs')
+
+  // Jobs page: Show the additional email lists when the user types in their email address
+  if(isJobsPage) {
+    const emailFieldJobs = document.querySelector('.email-form--jobs input[type=email]')
+
+    emailFieldJobs.addEventListener("keyup", () => {
+      $('.form-list-add').fadeIn(1000);
+    });
+  };
 
   form.addEventListener('submit', event => {
     event.preventDefault() // Stop form from submitting normally
@@ -9,7 +19,7 @@
       .then(response => console.log("Email subscription choices submitted."))
       .catch(error => console.error("Looks like there's an error.", error.message))
 
-    document.getElementById('form').style.display = 'none'; // make form invisible
-    document.getElementById('form-message').style.display = 'block'; // completion message
+    document.querySelector('#form').style.display = 'none'; // make form invisible
+    $('#form-message').fadeIn(1000); // completion message
   })
 })();
