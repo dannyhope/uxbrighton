729c8347962b5ccc6d885bf3c2ec897c7c7debca
Bruce Taylor <brootaylor@gmail.com>
2023-05-04 00:32:10 +0100

    Added email subscription form to past events pages

--- scripts/components/emailSubscription.js
+++ scripts/components/emailSubscription.js
@@ -1,13 +1,14 @@
 (function() {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbyFLPMiKDWQbsrwnbdIAnxL2GzvAv0hlKJrXypsZbCQ_4BIGWzFPhDHGJJTOkNCznXn/exec' // Google Apps Script URL
   const form = document.forms['google-sheet']
-  const isEmailHomePage = document.querySelector('.email-subscription--home')
-  const isEmailSubscribePage = document.querySelector('.email-subscription--subscribe')
-  const isEmailJobsPage = document.querySelector('.email-subscription--jobs')
+  const homePage = document.querySelector('.email-subscription--home')
+  const subscribePage = document.querySelector('.email-subscription--subscribe')
+  const jobsPage = document.querySelector('.email-subscription--jobs')
+  const eventsPage = document.querySelector('.email-subscription--events')
 
   // Home page: Show the additional email lists when the user types in their email address
-  if(isEmailHomePage) {
-    const emailFieldHome = document.querySelector('.email-subscription--home input[type=email]')
+  if(homePage) {
+    const emailFieldHome = homePage.querySelector('input[type=email]')
 
     emailFieldHome.addEventListener("keyup", () => {
       $('.form-list-add').fadeIn(1000);
@@ -15,10 +16,19 @@
   };
 
   // Jobs page: Show the additional email lists when the user types in their email address
-  if(isEmailJobsPage) {
-    const emailFieldJobs = document.querySelector('.email-subscription--jobs input[type=email]')
+  if(jobsPage) {
+    const emailFieldJobs = jobsPage.querySelector('input[type=email]')
 
     emailFieldJobs.addEventListener("keyup", () => {
+      $('.form-list-add').fadeIn(1000);
+    });
+  };
+
+  // Past events page: Show the additional email lists when the user types in their email address
+  if(eventsPage) {
+    const emailFieldEvents = eventsPage.querySelector('input[type=email]')
+
+    emailFieldEvents.addEventListener("keyup", () => {
       $('.form-list-add').fadeIn(1000);
     });
   };
@@ -34,8 +44,8 @@
       .catch(error => console.error("Looks like there's an error.", error.message))
 
     // Handle the subscribe page email subscription form a little differently
-    if(isEmailSubscribePage) {
-      document.querySelector('.form-content').style.display = 'none'; // make all other content invisible
+    if(subscribePage) {
+      subscribePage.querySelector('.form-content').style.display = 'none'; // make all other content invisible
     } else {
       document.querySelector('#form').style.display = 'none'; // make only the form invisible
     }
