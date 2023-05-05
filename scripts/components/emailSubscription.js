05f8e41772d5febcb0f2ec6161b15be372ed4197
Bruce Taylor <brootaylor@gmail.com>
2023-05-05 18:33:06 +0100

    Updated email subscription JS to check for form first

--- scripts/components/emailSubscription.js
+++ scripts/components/emailSubscription.js
@@ -33,23 +33,26 @@
     });
   };
 
-  // What to do when the form is submitted
-  form.addEventListener('submit', event => {
-    event.preventDefault() // Stop form from submitting normally
+  // Check if the form exists on the page first before adding the event listener
+  if(form) {
+    // What to do when the form is submitted
+    form.addEventListener('submit', event => {
+      event.preventDefault() // Stop form from submitting normally
 
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
 
-      // Console log the response from the Google Apps Script
-      .then(response => console.log("Email subscription choices submitted.", response.message))
-      .catch(error => console.error("Looks like there's an error.", error.message))
+        // Console log the response from the Google Apps Script
+        .then(response => console.log("Email subscription choices submitted.", response.message))
+        .catch(error => console.error("Looks like there's an error.", error.message))
 
-    // Handle the subscribe page email subscription form a little differently
-    if(subscribePage) {
-      subscribePage.querySelector('.form-content').style.display = 'none'; // make all other content invisible
-    } else {
-      document.querySelector('#form').style.display = 'none'; // make only the form invisible
-    }
+      // Handle the subscribe page email subscription form a little differently
+      if(subscribePage) {
+        subscribePage.querySelector('.form-content').style.display = 'none'; // make all other content invisible
+      } else {
+        document.querySelector('#form').style.display = 'none'; // make only the form invisible
+      }
 
-    $('.form-message').fadeIn(1000); // completion message fadein
-  })
+      $('.form-message').fadeIn(1000); // completion message fadein
+    })
+  }
 })();
