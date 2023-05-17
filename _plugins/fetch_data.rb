864f6426ecddb7c785572f1fe976d47a2e26ede1
Bruce Taylor <brootaylor@gmail.com>
2023-05-17 17:16:23 +0100

    Plugin update

--- _plugins/fetch_data.rb
+++ _plugins/fetch_data.rb
@@ -6,19 +6,19 @@ module FetchData
     def generate(site)
       # auth_token = process.env.JOBS_API_TOKEN # Replace with your authentication token
       # auth_token = '5e21833f-f5ea-4ce7-7a5c-2bfb537ca8bd'
-      auth_token = ENV['JOBS_API_TOKEN']
+      # auth_token = ENV['JOBS_API_TOKEN']
 
       # Iterate through each job post
       site.jobs.docs.each do |job|
         # Make API request for each job post and retrieve JSON data
         # uri = URI("https://workml.io/v1/jobs/{id}") # Modify the URL pattern according to your API
-        # uri = URI("https://jsonplaceholder.typicode.com/todos/1")
-        uri = URI("https://workml.io/v1/jobs/#{job.data['id']}") # Modify the URL pattern according to your API
+        uri = URI("https://jsonplaceholder.typicode.com/todos/1")
+        # uri = URI("https://workml.io/v1/jobs/#{job.data['id']}") # Modify the URL pattern according to your API
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true # Enable SSL if needed
 
         request = Net::HTTP::Get.new(uri)
-        request['Authorization'] = "Bearer #{auth_token}" # Add the authentication token as a Bearer token
+        # request['Authorization'] = "Bearer #{auth_token}" # Add the authentication token as a Bearer token
 
         response = http.request(request)
 
