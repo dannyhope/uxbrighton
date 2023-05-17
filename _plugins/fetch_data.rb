9ea5caf67669cf8e2c24c6dfd9c286d725183270
Bruce Taylor <brootaylor@gmail.com>
2023-05-17 17:36:00 +0100

    Trying to get Jekyll plugin working

--- _plugins/fetch_data.rb
+++ _plugins/fetch_data.rb
@@ -1,14 +1,28 @@
 require 'net/http'
+require 'json'
 
 module FetchData
   class Generator < Jekyll::Generator
     def generate(site)
-      # Make API request and retrieve JSON data
-      uri = URI('https://jsonplaceholder.typicode.com/todos/1')
-      response = Net::HTTP.get(uri)
+      # Modify the API endpoint and authentication as needed
+      api_url = 'https://jsonplaceholder.typicode.com/posts'
+      # auth_token = 'your_auth_token' # Replace with your authentication token
 
-      # Inject JSON data into site payload
-      site.data['api_data'] = response
+      uri = URI(api_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
+
+      request = Net::HTTP::Get.new(uri)
+      # request['Authorization'] = "Bearer #{auth_token}"
+
+      response = http.request(request)
+
+      if response.code == '200'
+        data = JSON.parse(response.body)
+        site.data['api_data'] = data
+      else
+        Jekyll.logger.warn "Failed to fetch API data"
+      end
     end
   end
 end
